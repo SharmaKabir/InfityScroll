4e665a5acb299ff2d1b7fd90f9e114bee2969e4a
SharmaKabir <sharmakabir204@gmail.com>
2024-03-30 00:08:20 +0530

    update script and styling

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 //https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
-const count= 10;
+let intialLoad=true;
+let count= 5;
 const apiKey='Ef_Q2eQA1uVRmtOUcYWj7C2G649eS-gricOM8Vem84Q';
 const apiUrl=`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 const imageContainer = document.getElementById('image-container');
 const loader=document.getElementById('loader');
+let ready=false;
+let imagesLoaded=0;
+let totalImages=0;
 let photosArray=[];
 
 
+
+
+
+
+//check if all images were loaded
+ function imageLoaded(){
+   
+    imagesLoaded++;
+    console.log(imagesLoaded);
+    if(imagesLoaded===totalImages){
+        ready=true;
+        loader.hidden=true;
+        console.log("ready", ready);
+        intialLoad=false;
+        count=30;
+    }
+ }
 
 
 
@@ -16,8 +37,7 @@ let photosArray=[];
 //key is src,alt,href
 function setAttributes(element,attributes){
     for(const key in attributes){
-        element.setAttribute(key,attributes[key]);
-        
+        element.setAttribute(key,attributes[key]);  
     }
 }
 
@@ -34,9 +54,13 @@ function setAttributes(element,attributes){
 
 //response.json=photosArray
 //then loop for each photo edit the img
-//get photos from unsplash a
+//get photos from unsplash api
 //create elements 
+
 function displayPhotos(){
+    imagesLoaded=0;
+    totalImages=photosArray.length;
+console.log('total images', totalImages);
 photosArray.forEach((photo)=>{
 //create element
 //create anchor to link unsplash
@@ -44,19 +68,28 @@ const item=document.createElement('a');
 // item.setAttribute('href',photo.links.html);
 // item.setAttribute('target','_blank');
 //create img for photo
+
 setAttributes(item,{
 href:photo.links.html,
 target:'_blank',
 });
+
 const img=document.createElement("img");
+
 //img.setAttribute('src',photo.urls.regular);
 // img.setAttribute('alt',photo.alt_description);
 // img.setAttribute('title',photo.alt_description);
+
 setAttributes(img,{
 src:photo.urls.regular,
 alt:photo.alt_description,
 title:photo.alt_description,
 });
+//check when each image is finished loaded
+img.addEventListener('load',imageLoaded);
+
+
+
 //put img inside anchor, then put both inside container
 item.appendChild(img);
 imageContainer.appendChild(item);
@@ -81,12 +114,11 @@ async function getPhotos(){
 //check if scrolling near bottom of page to load more pages
 window.addEventListener('scroll',()=>{
 console.log('scrolled');
-if(window.innerHeight + window.scrollY >= document.body.offsetHeight -1000){
+if(window.innerHeight + window.scrollY >= document.body.offsetHeight -1000 && ready){
+    ready=false;
     getPhotos();
     console.log("LOAD MORE");
 }
-
-
 });
 
 
